81baea9d03d549758689608a3d06ef3b778a1640
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-08-28 18:32:34 +0900

    add code

--- composables/webgl.ts
+++ composables/webgl.ts
@@ -1,9 +1,27 @@
+const createShader = (gl, type, source) => {
+  const shader = gl.createShader(type)
+  if (shader === null) {
+    console.error('Failed to create a shader')
+    return null
+  }
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+  if (!compiled) {
+    const log = gl.getShaderInfoLog(shader)
+    console.error('Failed to compile a shader\n' + log)
+    gl.deleteShader(shader)
+    return null
+  }
+  return shader
+}
+
 export const useWebGl = (canvas) => {
   const gl = ref()
   onMounted(() => {
     canvas.value.width = 500
     canvas.value.height = 300
-    gl.value = canvas.value.getContext('webgl') || canvas.value.getContext('experimental-webgl')
+    gl.value = canvas.value.getContext('webgl')
     gl.value.clearColor(.0, .0, .0, 1.)
     gl.value.clear(gl.value.COLOR_BUFFER_BIT)
   })
