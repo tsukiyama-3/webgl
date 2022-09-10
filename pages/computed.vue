e9b4b511aaa9c5c37c174268af9ea21e22690dad
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-10 14:11:33 +0900

    add computed.vue

---
+++ pages/computed.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const count = ref(1)
+const plusOne = computed(() => count.value + 1)
+console.log(plusOne.value)
+</script>
+
+<template>
+	<div>
+		<h1>Computed</h1>
+		<p>{{ count }}</p>
+	</div>
+</template>
