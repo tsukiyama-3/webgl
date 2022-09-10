e239d4f2b8427a68563e0f02ee3f89f1cc4a9dd0
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-10 22:01:00 +0900

    add resolve

--- pages/promise.vue
+++ pages/promise.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-const promise = new Promise((resolve, reject) => { })
+const promise = new Promise(resolve => {
+	resolve()
+}).then(() => [console.log('resolveしたよ')])
 console.log(promise)
 </script>
 
 <template>
-  <div>
-    <h1>Promise</h1>
-  </div>
+	<div>
+		<h1>Promise</h1>
+	</div>
 </template>
