768967e40e54fbb898eea549878c9be4fe4c4521
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-10 21:47:35 +0900

    add promise instance

--- pages/promise.vue
+++ pages/promise.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
-console.log(1)
-// お約束を取り付けたい処理にPromise
-new Promise((resolve) => {
-  // 1秒後に実行する処理
-  setTimeout(() => {
-    console.log(2)
-    // 無事処理が終わったことを伝える
-    resolve()
-  }, 1000)
-}).then(() => {
-  // 処理が無事に終わったことを受け取って実行される処理
-  console.log(3)
-})
+const promise = new Promise((resolve, reject) => { })
+console.log(promise)
 </script>
 
 <template>
