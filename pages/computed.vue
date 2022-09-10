532ee33f16f04cfcd87c2fb901fa1c9a00c6f1a6
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-10 15:22:42 +0900

    add runtime config

--- pages/computed.vue
+++ pages/computed.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-const count = ref(1)
-const plusOne = computed(() => count.value + 1)
-console.log(plusOne.value)
+const runtimeConfig = useRuntimeConfig()
+onMounted(() => {
+	console.log(runtimeConfig.hoge)
+})
+console.log(runtimeConfig.hoge)
 </script>
 
 <template>
 	<div>
 		<h1>Computed</h1>
-		<p>{{ count }}</p>
 	</div>
 </template>
