d42e5e440db910c32fca2769b83533c20bb6b4bf
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-11 12:22:05 +0900

    add Promise.all

--- pages/promise.vue
+++ pages/promise.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-const promise = new Promise((resolve, reject) => {
-	reject('test')
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve()
+  }, 1000)
+}).then(() => {
+  console.log('promise1終わったよ')
 })
-	.then(val => {
-		console.log(`then1: ${val}`)
-		return val
-	})
-	.catch(val => {
-    console.log(`catch: ${val}`)
-    return val
-	})
-	.then(val => {
-		console.log(`then2: ${val}`)
-	})
+
+const promise2 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve()
+  }, 3000)
+}).then(() => {
+  console.log('promise2終わったよ')
+})
+
+Promise.all([promise1, promise2]).then(() => {
+  console.log('全部終わったよ')
+})
 </script>
 
 <template>
