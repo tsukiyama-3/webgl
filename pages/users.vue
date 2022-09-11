e579819a5222b2722b29c19fbfad5844e3810d72
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-11 21:32:01 +0900

    add users

---
+++ pages/users.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data: users } = await useFetch('/api/users')
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.username }}
+      </li>
+    </ul>
+  </div>
+</template>
