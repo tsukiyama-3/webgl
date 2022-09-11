4306db66f306963fbb9d40d9c64ae1cd3f0060e9
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-11 21:21:46 +0900

    modify api

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,1 +1,37 @@
-export default (req, res) => 'Hello Nuxt3 from server!'
+import type { IncomingMessage, ServerResponse } from 'http'
+
+export interface User {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: Address
+  phone: string
+  website: string
+  company: Company
+}
+
+export interface Address {
+  street: string
+  suite: string
+  city: string
+  zipcode: string
+  geo: Geo
+}
+
+export interface Geo {
+  lat: string
+  lng: string
+}
+
+export interface Company {
+  name: string
+  catchPhrase: string
+  bs: string
+}
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+  const result: User[] = await $fetch('https://jsonplaceholder.typicode.com/users')
+
+  return result
+}
