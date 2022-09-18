37ce677c2f9222e0fc264fa68eb69fced2b0c34b
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-09-18 23:40:38 +0900

    refactoring

--- composables/dot-sample.ts
+++ composables/dot-sample.ts
@@ -4,19 +4,18 @@ import { VSHADER_CODE } from './shader/vertex-shader'
 import { FSHADER_CODE } from './shader/fragment-shader'
 
 export const useDotSmaple = (canvas: Ref<HTMLCanvasElement>) => {
-  const gl = ref()
   onMounted(() => {
     canvas.value.width = 300
     canvas.value.height = 300
-    gl.value = canvas.value.getContext('webgl2')
-    if (!gl.value) {
+    const gl = canvas.value.getContext('webgl2')
+    if (!gl) {
       console.error('Faild to obtain WebGL 2.0 context')
       return
     }
     console.log(VSHADER_CODE)
-    const program = createProgramFromCode(gl.value, VSHADER_CODE, FSHADER_CODE)
-    gl.value.useProgram(program)
-    render(gl.value)
+    const program = createProgramFromCode(gl, VSHADER_CODE, FSHADER_CODE)
+    gl.useProgram(program)
+    render(gl)
   })
   const render = (gl) => {
     gl.clearColor(0, 0, .5, 1.)
